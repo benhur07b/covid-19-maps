1ec1725142d268953c201f9930728b7cbae7cca2
benhur07b <benhur07b@users.noreply.github.com>
2020-04-01 06:14:37 +0800

    update

--- facilities-with-cases/static/js/main.js
+++ facilities-with-cases/static/js/main.js
@@ -1,16 +1,6 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYm5ociIsImEiOiJjazg2MXpuaTUwZDkzM2VycTJ3Nzd1aWJhIn0.m5u8w_MFie9bNVm_sIS4nw'
 
-// var facilities = $.ajax({
-//     url: 'https://services5.arcgis.com/mnYJ21GiFTR97WFg/ArcGIS/rest/services/conf_fac_tracking/FeatureServer/0/query?where=facility%3C%3E%27For%20validation%27&outFields=*&f=pgeojson',
-//     dataType: 'json'
-// })
-
-// $(document).ready(function() {
-$.getJSON('https://services5.arcgis.com/mnYJ21GiFTR97WFg/ArcGIS/rest/services/conf_fac_tracking/FeatureServer/0/query?where=facility%3C%3E%27For%20validation%27&outFields=*&f=pgeojson', function(facilities) {
-    // console.log(facilities)
-
-    var filterBtn = document.getElementById('filter-btn');
-    var clearBtn = document.getElementById('clear-btn');
+$(document).ready(function() {
 
     var map = new mapboxgl.Map({
         container: 'map',
@@ -24,14 +14,14 @@ $.getJSON('https://services5.arcgis.com/mnYJ21GiFTR97WFg/ArcGIS/rest/services/co
       .domain(['low', 'moderate', 'high', 'very high', 'critical'])
       .range(num_colors)
 
-    map.addControl(new mapboxgl.NavigationControl(), 'top-left');
+    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 
     map.on('load', () => {
         map.addSource('facilities', {
             'type': 'geojson',
             // 'data': 'https://services5.arcgis.com/mnYJ21GiFTR97WFg/ArcGIS/rest/services/conf_fac_tracking/FeatureServer/0/query?where=count_%3E0&outFields=*&f=pgeojson',
-            // 'data': 'https://services5.arcgis.com/mnYJ21GiFTR97WFg/ArcGIS/rest/services/conf_fac_tracking/FeatureServer/0/query?where=facility%3C%3E%27For%20validation%27&outFields=*&f=pgeojson',
-            'data': facilities,
+            'data': 'https://services5.arcgis.com/mnYJ21GiFTR97WFg/ArcGIS/rest/services/conf_fac_tracking/FeatureServer/0/query?where=facility%3C%3E%27For%20validation%27&outFields=*&f=pgeojson',
+            // 'data': facilities,
             'cluster': true,
             'clusterRadius': 60,
             'clusterProperties': {
@@ -142,7 +132,7 @@ $.getJSON('https://services5.arcgis.com/mnYJ21GiFTR97WFg/ArcGIS/rest/services/co
            }
        });
 
-       buildFacilityList(facilities);
+       buildFacilityList();
 
        map.on('click', 'facility_cluster', function(e) {
            var features = map.queryRenderedFeatures(e.point, {
@@ -179,32 +169,6 @@ $.getJSON('https://services5.arcgis.com/mnYJ21GiFTR97WFg/ArcGIS/rest/services/co
             .setHTML(popup)
             .addTo(map);
         });
-
-        filterBtn.onclick = function() {
-            var num = Number(document.getElementById('num-text').value);
-            var comp = getComp(document.getElementById('comp-text').value);
-            if (num && comp){
-                map.setFilter('facility', ['all', [comp, 'count_', num]]);
-                map.setFilter('facility_outer', ['all', [comp, 'count_', num]]);
-                // map.setFilter('facility_cluster', ['all', [comp, ['get', 'point_count'], num], ['has', 'point_count']]);
-                // map.setFilter('facility_cluster_count', ['all', [comp, ['get', 'point_count'], num], ['has', 'point_count']]);
-            } else {
-                map.setFilter('facility', ['!=', ['get', 'cluster'], true],);
-                map.setFilter('facility_outer', ['!=', ['get', 'cluster'], true],);
-                // map.setFilter('facility_cluster', ['has', 'point_count']);
-                // map.setFilter('facility_cluster_count', ['has', 'point_count']);
-            }
-        };
-
-        clearBtn.onclick = function() {
-            document.getElementById('num-text').value = '';
-            document.getElementById('comp-text').value = '';
-
-            map.setFilter('facility', ['!=', ['get', 'cluster'], true],);
-            map.setFilter('facility_outer', ['!=', ['get', 'cluster'], true],);
-            // map.setFilter('facility_cluster', ['has', 'point_count']);
-            // map.setFilter('facility_cluster_count', ['has', 'point_count']);
-        };
 
         // Change the cursor to a pointer when the mouse is over the places layer.
         map.on('mouseenter', 'facility', function() {
@@ -244,44 +208,46 @@ $.getJSON('https://services5.arcgis.com/mnYJ21GiFTR97WFg/ArcGIS/rest/services/co
         .addTo(map);
     }
 
-    function buildFacilityList(data) {
-        data.features.forEach(function(facility, i){
-            var prop = facility.properties;
+    function buildFacilityList() {
+        $.getJSON('https://services5.arcgis.com/mnYJ21GiFTR97WFg/ArcGIS/rest/services/conf_fac_tracking/FeatureServer/0/query?where=facility%3C%3E%27For%20validation%27&outFields=*&f=pgeojson', function(facilities) {
+            facilities.features.forEach(function(facility, i){
+                var prop = facility.properties;
 
-            /* Add a new listing section to the sidebar. */
-            var listings = document.getElementById('listings');
-            var listing = listings.appendChild(document.createElement('div'));
-            /* Assign a unique `id` to the listing. */
-            listing.id = 'listing-' + prop.ObjectId;
-            /* Assign the `item` class to each listing for styling. */
-            listing.className = 'item';
+                /* Add a new listing section to the sidebar. */
+                var listings = document.getElementById('listings');
+                var listing = listings.appendChild(document.createElement('div'));
+                /* Assign a unique `id` to the listing. */
+                listing.id = 'listing-' + prop.ObjectId;
+                /* Assign the `item` class to each listing for styling. */
+                listing.className = 'item';
 
-            /* Add the link to the individual listing created above. */
-            var link = listing.appendChild(document.createElement('a'));
-            link.href = '#';
-            link.className = 'title';
-            link.id = 'link-' + prop.ObjectId;
-            link.innerHTML = prop.facility;
+                /* Add the link to the individual listing created above. */
+                var link = listing.appendChild(document.createElement('a'));
+                link.href = '#';
+                link.className = 'title';
+                link.id = 'link-' + prop.ObjectId;
+                link.innerHTML = prop.facility;
 
-            link.addEventListener('click', function(e) {
-                // console.log(this.id)
-                for (var i = 0; i < facilities.features.length; i++) {
-                  if (this.id === 'link-' + facilities.features[i].properties.ObjectId) {
-                    var clickedListing = facilities.features[i];
-                    flyToFacility(clickedListing);
-                    showPopup(clickedListing);
-                  }
-                }
-                    var activeItem = document.getElementsByClassName('active');
-                    if (activeItem[0]) {
-                    activeItem[0].classList.remove('active');
+                link.addEventListener('click', function(e) {
+                    // console.log(this.id)
+                    for (var i = 0; i < facilities.features.length; i++) {
+                      if (this.id === 'link-' + facilities.features[i].properties.ObjectId) {
+                        var clickedListing = facilities.features[i];
+                        flyToFacility(clickedListing);
+                        showPopup(clickedListing);
+                      }
                     }
-                this.parentNode.classList.add('active');
-            });
+                        var activeItem = document.getElementsByClassName('active');
+                        if (activeItem[0]) {
+                        activeItem[0].classList.remove('active');
+                        }
+                    this.parentNode.classList.add('active');
+                });
 
-            /* Add details to the individual listing. */
-            var details = listing.appendChild(document.createElement('div'));
-            details.innerHTML = `Cases: ${prop.count_}`;
+                /* Add details to the individual listing. */
+                var details = listing.appendChild(document.createElement('div'));
+                details.innerHTML = `Cases: ${prop.count_}`;
+            });
         });
     }
 
@@ -300,4 +266,32 @@ $.getJSON('https://services5.arcgis.com/mnYJ21GiFTR97WFg/ArcGIS/rest/services/co
         }
     };
 
-})
+    var filterBtn = document.getElementById('filter-btn');
+    // var clearBtn = document.getElementById('clear-btn');
+
+    filterBtn.onclick = function() {
+        var num = Number(document.getElementById('num-text').value);
+        var comp = getComp(document.getElementById('comp-text').value);
+        if (num && comp){
+            map.setFilter('facility', ['all', [comp, 'count_', num]]);
+            map.setFilter('facility_outer', ['all', [comp, 'count_', num]]);
+            // map.setFilter('facility_cluster', ['all', [comp, ['get', 'point_count'], num], ['has', 'point_count']]);
+            // map.setFilter('facility_cluster_count', ['all', [comp, ['get', 'point_count'], num], ['has', 'point_count']]);
+        } else {
+            map.setFilter('facility', ['!=', ['get', 'cluster'], true],);
+            map.setFilter('facility_outer', ['!=', ['get', 'cluster'], true],);
+            // map.setFilter('facility_cluster', ['has', 'point_count']);
+            // map.setFilter('facility_cluster_count', ['has', 'point_count']);
+        }
+    };
+
+    // clearBtn.onclick = function() {
+    //     document.getElementById('num-text').value = '';
+    //     document.getElementById('comp-text').value = '';
+    //
+    //     map.setFilter('facility', ['!=', ['get', 'cluster'], true],);
+    //     map.setFilter('facility_outer', ['!=', ['get', 'cluster'], true],);
+    //     // map.setFilter('facility_cluster', ['has', 'point_count']);
+    //     // map.setFilter('facility_cluster_count', ['has', 'point_count']);
+    // };
+});
